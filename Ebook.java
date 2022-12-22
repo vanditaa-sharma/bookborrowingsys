c7ee1e2901b6c3d9c83fdbab22c7558e2d950f13
vanditaa-sharma <78145034+vanditaa-sharma@users.noreply.github.com>
2022-12-22 12:38:35 +0530

    Create Ebook.java

---
+++ Ebook.java
@@ -0,0 +1,8 @@
+public class Ebook extends Book{
+  private String format;
+  
+  Ebook(String title,String author,int pageCount, String format){
+    super(title, author, pageCount);
+    this.format=format;
+  }
+}
